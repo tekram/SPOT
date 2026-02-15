6f4985a5d721e51ae8434298971a4cfe5f915a97
Tashfeen Ekram <tashfeen.ekram@gmail.com>
2026-02-15 10:26:26 -0600

    fix

--- render-build.sh
+++ render-build.sh
@@ -7,18 +7,13 @@ npm install
 cat > config/config.json << CONFIGEOF
 {
   "secrets": {
+    "ACCESS_CODE": "${ACCESS_CODE}",
     "DATABASE_URL": "${DATABASE_URL}",
-    "TBA_API_KEY": "${TBA_API_KEY}",
-    "GOOGLE_AUTH": {
-      "CLIENT_ID": "${GOOGLE_CLIENT_ID}",
-      "CLIENT_SECRET": "${GOOGLE_CLIENT_SECRET}"
-    },
-    "FMS_API_USERNAME": "${FMS_API_USERNAME}",
-    "FMS_API_KEY": "${FMS_API_KEY}"
+    "TBA_API_KEY": "${TBA_API_KEY}"
   },
-  "EVENT_NUMBER": "${EVENT_NUMBER}",
-  "VERSION": "0.0",
-  "TBA_EVENT_KEY": "${TBA_EVENT_KEY}"
+  "TBA_EVENT_KEY": "${TBA_EVENT_KEY}",
+  "VERSION": "1.0",
+  "DEMO": false
 }
 CONFIGEOF
 
